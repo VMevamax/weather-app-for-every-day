5c22de3f7dd6f0337195a0f562fb956351b33b62
VMevamax <132147870+VMevamax@users.noreply.github.com>
2023-06-11 22:01:43 +0300

    search city Kyiv

--- temp1.js
+++ temp1.js
@@ -114,7 +114,7 @@ function handleSubmit(event) {
   let city = document.querySelector("#city-input").value;
   searchCity(city);
 }
-
+searchCity("kyiv");
 function searchLocation(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
